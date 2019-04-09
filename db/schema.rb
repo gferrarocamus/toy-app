a2332e47b6839a0deb78d684687bf3ade32fa886
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-09 18:15:15 +0200

    Add scaffolding for microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_409_144_459) do
+ActiveRecord::Schema.define(version: 20_190_409_160_440) do
+  create_table 'microposts', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
